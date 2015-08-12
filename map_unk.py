6da90e09e3c05143e66b0c9af7f5f1285be381fc
armatthews <armatthe@cmu.edu>
2015-08-12 04:39:31 -0400

    Added script to aid UNKing of corpora

---
+++ map_unk.py
@@ -0,0 +1,14 @@
+import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('vocab')
+parser.add_argument('-u', '--unk_token', required=False, default='UNK')
+args = parser.parse_args()
+
+vocab = set(map(lambda s: s.strip(), open(args.vocab).readlines()))
+
+for line in sys.stdin:
+	words = line.strip().split()
+	words = [word if word in vocab else args.unk_token for word in words]
+	print ' '.join(words)
